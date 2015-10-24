828f3008b5b7206e965d9300aae8fe53fedf8e3c
Peter Mitrano <mitranopeter@gmail.com>
2015-10-24 18:36:18 -0400

    Updated bluetooth sending structure
    also fixed some falsely tracked files

---
+++ app/src/main/java/com/example/ikeandmike/field/SendFieldRunnable.java
@@ -0,0 +1,42 @@
+package com.example.ikeandmike.field;
+
+import com.hoho.android.usbserial.driver.ProbeTable;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.List;
+
+/**
+ * Created by peter on 10/24/15.
+ */
+public class SendFieldRunnable implements  Runnable{
+
+    final private OutputStream os;
+
+    public SendFieldRunnable(OutputStream os){
+        this.os = os;
+    }
+
+    @Override
+    public void run(){
+        byte[] supplyData = new byte[1];
+        byte[] storageData = new byte[1];
+        byte data = FieldState.getInstance().getStorageSupplyByte();
+        storageData[0] = (byte)(data & 0x0F);
+        supplyData[0] = (byte)(data & 0xF0);
+
+        byte[] storagePacket = BTProtocol.createPacket(BTProtocol.Type.FIELD,
+                storageData);
+        byte[] supplPacket = BTProtocol.createPacket(BTProtocol.Type.FIELD,
+                storageData);
+
+        try {
+            os.write(supplPacket);
+            os.write(storagePacket);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
