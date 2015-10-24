ab8eade337bd14ce35fa257918ecfd38d2c6a05b
Peter Mitrano <mitranopeter@gmail.com>
2015-10-24 03:37:32 -0400

    renamed main activity and deleted example code

---
+++ app/src/main/java/com/example/ikeandmike/field/Main.java
@@ -0,0 +1,17 @@
+package com.example.ikeandmike.field;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * An example full-screen activity that shows and hides the system UI (i.e.
+ * status bar and navigation/system bar) with user interaction.
+ */
+public class Main extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fullscreen);
+    }
+}
