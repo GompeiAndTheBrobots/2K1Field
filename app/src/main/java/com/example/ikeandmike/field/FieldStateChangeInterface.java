f219db3955ff5ef3ae980824b84e39d210fd887b
engineeringawesome <robomower9508@gmail.com>
2015-10-24 22:27:07 -0400

    Created the interface

---
+++ app/src/main/java/com/example/ikeandmike/field/FieldStateChangeInterface.java
@@ -0,0 +1,10 @@
+package com.example.ikeandmike.field;
+
+/**
+ * Interface for objects that want events to happen when the field state
+ * changes
+ * Created by jordanbrobots on 10/24/15.
+ */
+public interface FieldStateChangeInterface {
+    void onFieldStateChange(Byte supplyStorageByte);
+}
